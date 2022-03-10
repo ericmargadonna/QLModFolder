5927e4c84a3948f7155f890b661281593bf541a9
ericmargadonna <eric082659@gmail.com>
2022-03-09 19:02:34 -0500

    okay, now I'm done

--- DLLInjectorMAC.bat
+++
@@ -1,7 +0,0 @@
-Rem This is a simple batch script that automagically moves my newly compiled 
-Rem dll's into the necessary .pak file for the purpose of Quake 4 Modding
-del gamex86.dll
-ren game000.pk4 game000.zip
-C:\"Program Files"\7-Zip\7z.exe d game000.zip Gamex86.dll 
-C:\"Program Files"\7-Zip\7z.exe a game000.zip C:\Users\ericmargadonna\Development\Win32\Release\Gamex86.dll 
-ren game000.zip game000.pk4
